153ca4c08b7ea381b17f98b5670c7cdd29325044
Tim Dodge <timldodge@gmail.com>
2018-06-18 10:53:02 -0700

    api finished

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const User = use('App/Model/User')
+const Hash = use('Hash')
+const Tweet = use('App/Models/User')
 
 class UserController {
 
@@ -44,6 +46,183 @@ class UserController {
         message: 'Invalid email/password.'
       })
     }
+  }
+
+  async me ({ auth, response }) {
+    // https://scotch.io/courses/build-a-twitter-clone-with-adonis-and-vue/update-user-profile
+    const user = await User.query()
+      .where('id', auth.current.user.id)
+      .with('tweets', builder => {
+        builder.with('user')
+        builder.with('favorites')
+        builder.with('replies')
+      })
+      .with('following')
+      .with('followers')
+      .with('favorites')
+      .with('favorites.tweet', builder => {
+        builder.with('user')
+        builder.with('favorites')
+        builder.with('replies')
+      })
+      .firstOrFail()
+
+    return response.json({
+      status: 'success',
+      data: user
+    })
+  }
+
+  async updateProfile ({ request, auth, response }) {
+
+    try {
+      // get currently authenticated user
+      const user = auth.current.user
+      // update with new data entered
+      user.name = request.input('name')
+      user.username = request.input('username')
+      user.email = request.input('email')
+      user.location = request.input('location')
+      user.bio = request.input('bio')
+      user.website_url = request.input('website_url')
+
+      await user.save()
+
+      return response.json({
+        status: 'success',
+        message: 'Profile updated!',
+        data: user
+      })
+    } catch (error) {
+      return response.status(400).json({
+        status: 'error',
+        message: 'There was a problem updating the profile, please try again.'
+      })
+    }
+  }
+
+  async changePassword ({ request, auth, response }) {
+    // get current authenticated user
+    const user = auth.current.user
+
+    // verify if current password matches
+    const verifyPassword = await Hash.verify(
+      request.input('password'),
+      user.password
+    )
+
+    // display wrong password message
+    if (!verifyPassword) {
+      return response.status(400).json({
+        status: 'error',
+        message: 'Current password could not be verified. Please try again.'
+      })
+    }
+
+    user.password = await Hash.make(request.input('newPassword'))
+    await user.save()
+
+    return response.json({
+      status: 'success',
+      message: 'Password updated!'
+    })
+  }
+
+  async showProfile ({ request, params, response }) {
+    try {
+      const user = await User.query()
+        .where('username', params.username)
+        .with('tweets', builder => {
+          builder.with('user')
+          builder.with('favorites')
+          builder.with('replies')
+        })
+        .with('following')
+        .with('follwers')
+        .with('favorites')
+        .with('favorites.tweet', builder => {
+          builder.with('user')
+          builder.with('favorites')
+          builder.with('replies')
+        })
+        .firstOrFail()
+
+      return response.json({
+        status: 'success',
+        data: user
+      })
+    } catch {
+      status: 'error',
+      message: 'User not found.'
+    }
+  }
+
+  async usersToFollow ({ params, auth, response }) {
+    // get current authenticated user
+    const user = auth.current.user
+
+    // get the ID's of users the currently authenticated user is already following
+    const usersAlreadyFollowing = await user.following().ids()
+
+    // fetch users the currently authenticated user is not following
+    const usersToFollow = await User.query()
+      .whereNot('id', user.id)
+      .whereNotIn('id', usersAlreadyFollowing)
+      .pick(3)
+
+    return response.json({
+      status: 'success',
+      data: usersToFollow
+    })
+  }
+
+  async follow ({ request, auth, response }) {
+    // get current auth'd user
+    const user = auth.current.user
+
+    // add to user's followers
+    await user.following().attach(request.input('user_id'))
+
+    return response.json({
+      status: 'success',
+      data: null
+    })
+  }
+
+  async unFollow ({ params, auth, response }) {
+    // current user
+    const user = auth.current.user
+
+    // remove from user's followers
+    await user.following().detach(params.id)
+
+    return response.json({
+      status: 'success',
+      data: null
+    })
+  }
+
+  async timeline ({ auth, response }) {
+    const user = await User.find(auth.current.user.id) // TODO this may break. See line 212 for why
+
+    // get an array of IDs of the users followed by current user
+    const followersIds = await user.following().ids()
+
+    // add the user's ID to the array
+    followersIds.push(user.id)
+
+    const tweets = await Tweet.query()
+      .whereIn('user_id', followersIds)
+      .with('user')
+      .with('favorites')
+      .with('replies')
+      .fetch()
+
+    return response.json({
+      status: 'success',
+      data: tweets
+    })
+
   }
 
 }
--- start/routes.js
+++ start/routes.js
@@ -20,5 +20,36 @@ Route.get('/', ({ request }) => {
 })
 
 Route.post('/signup', 'UserController.signup')
-
 Route.post('/login', 'UserController.login')
+
+Route.group( () => {
+  Route.get('/me', 'UserController.me')
+  Route.put('/update_profile', 'UserController.updateProfile')
+})
+  .prefix('account')
+  .middleware(['auth:jwt'])
+
+Route.put('/change_password', 'UserController.changePassword')
+
+Route.group( () => {
+  Route.get('/users_to_follow', 'UserController.usersToFollow')
+  Route.post('/follow/:id', 'UserController.follow')
+  Route.delete('/unfollow/:id', 'UserController.unFollow')
+  Route.get('/timeline', 'UserController.')
+})
+  .prefix('users')
+  .middleware(['auth:jwt'])
+
+Route.post('/tweet', 'TweetController.tweet').middleware(['auth:jwt'])
+Route.get('/tweets/:id', 'TweetController.show')
+Route.post('/tweets/reply/:id', 'TweetController.reply').middleware(['auth:jwt'])
+Route.delete('/tweets/destroy/:id', 'TweetController.destroy').middleware(['auth:jwt'])
+
+Route.group( () => {
+  Route.post('/create', 'FavoriteController.favorite')
+  Route.delete('/destroy/:id', 'FavoriteController.unFavorite')
+})
+  .prefix('favorites')
+  .middleware(['auth:jwt'])
+
+Route.get(':username', 'UserController.showProfile')
